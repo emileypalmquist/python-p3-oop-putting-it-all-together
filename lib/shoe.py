857d405440893c0c14910877f8166e91a865c619
Emiley Palmquist <54187547+emileypalmquist@users.noreply.github.com>
2022-11-07 10:30:33 -0800

    tests passing

--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,10 @@
 #!/usr/bin/env python3
 
 class Shoe:
-    pass
+    def __init__(self, brand, condition="New"):
+        self.brand = brand
+        self.condition = condition
+
+    def cobble(self):
+        self.condition = "New"
+        print("Your shoe is as good as new!")
